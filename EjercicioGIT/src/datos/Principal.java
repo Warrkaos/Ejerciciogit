db02481889fae9a277e3e0a9f9fa7eae30c1a93f
AlvaroBR <AlvaroBR@ABR>
2017-10-05 13:06:34 +0200

    Mensaje de cambios en el protecto.

---
+++ EjercicioGIT/src/datos/Principal.java
@@ -0,0 +1,13 @@
+package datos;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	private static Scanner sc= new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ejercicio GitHub");
+	}
+
+}
