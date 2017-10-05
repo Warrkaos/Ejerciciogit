24be35c7e94c6355b6d7d3f739c7c63efebba985
AlvaroBR <AlvaroBR@ABR>
2017-10-05 13:30:48 +0200

    syso de cambios

--- EjercicioGIT/src/datos/Principal.java
+++ EjercicioGIT/src/datos/Principal.java
@@ -8,6 +8,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ejercicio GitHub");
+		System.out.println("Cambios...");
 	}
 
 }
